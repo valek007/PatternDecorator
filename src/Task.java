f01f1711d3104c6269d65e1c2176e37534b706bf
valek <coronadev.king@gmail.com>
2020-05-28 23:48:50 +0300

    Create main class Task and initialize Developer object

---
+++ src/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+
+    public static void main(String[] args) {
+
+        Developer developer = new JavaDeveloper();
+
+        System.out.println(developer.makeJob());
+    }
+}
